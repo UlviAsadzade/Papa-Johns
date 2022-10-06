893d6c68e76949284319b438d870545273583e64
ulvi1993 <ulvi.esedzadeh@gmail.com>
2022-10-06 18:46:04 +0400

    created header account

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+const optionMenu = document.querySelector(".select-menu"),
+  selectBtn = optionMenu.querySelector(".select-btn"),
+  options = optionMenu.querySelectorAll(".option"),
+  sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+selectBtn.addEventListener("click", () =>
+  optionMenu.classList.toggle("active")
+);
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    let selectedOption = option.querySelector(".option-text").innerText;
+
+    optionMenu.classList.remove("active");
+  });
+});
