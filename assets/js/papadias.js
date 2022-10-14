816a28d4d8671be6fcd2b67158f374175adebafe
ulvi1993 <ulvi.esedzadeh@gmail.com>
2022-10-14 20:52:22 +0400

    created papadias page with responsive

---
+++ assets/js/papadias.js
@@ -0,0 +1,30 @@
+let papadiasMinus = document.querySelectorAll('.papadias-minus');
+let papadiasPlus = document.querySelectorAll('.papadias-plus');
+
+papadiasMinus.forEach(min => {
+  let prdPrice = min.parentElement.nextElementSibling.children[0].innerText;
+  min.onclick = function () {
+    count = min.nextElementSibling.innerText;
+    if (count == 1) {
+      min.nextElementSibling.innerText = count;
+    } else {
+      count--;
+      min.nextElementSibling.innerText = count;
+    }
+    prdTotalPrice = (prdPrice * count).toFixed(2);
+    min.parentElement.nextElementSibling.children[0].innerText = prdTotalPrice;
+    
+  }
+})
+
+papadiasPlus.forEach(pl => {
+  let prdPrice = pl.parentElement.nextElementSibling.children[0].innerText;
+  pl.onclick = function () {
+    count = pl.previousElementSibling.innerText;
+    count++;
+    pl.previousElementSibling.innerText = count
+    prdTotalPrice = (prdPrice * count).toFixed(2);
+    pl.parentElement.nextElementSibling.children[0].innerText = prdTotalPrice;
+    
+  }
+})
