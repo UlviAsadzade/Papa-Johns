301610cdff989bd3d8b4898874ab16439bbe11e2
ulvi1993 <ulvi.esedzadeh@gmail.com>
2022-10-23 15:02:48 +0400

    pasta page created with responsive

---
+++ assets/js/pasta.js
@@ -0,0 +1,30 @@
+let pastaMinus = document.querySelectorAll('.pasta-minus');
+let pastaPlus = document.querySelectorAll('.pasta-plus');
+
+pastaMinus.forEach(min => {
+  let prdPrice = min.parentElement.nextElementSibling.children[0].innerText;
+  min.onclick = function () {
+    count = min.nextElementSibling.innerText;
+    if (count == 1) {
+      min.nextElementSibling.innerText = count;
+    } else {
+      count--;
+      min.nextElementSibling.innerText = count;
+    }
+    prdTotalPrice = (prdPrice * count).toFixed(2);
+    min.parentElement.nextElementSibling.children[0].innerText = prdTotalPrice;
+    
+  }
+})
+
+pastaPlus.forEach(pl => {
+  let prdPrice = pl.parentElement.nextElementSibling.children[0].innerText;
+  pl.onclick = function () {
+    count = pl.previousElementSibling.innerText;
+    count++;
+    pl.previousElementSibling.innerText = count
+    prdTotalPrice = (prdPrice * count).toFixed(2);
+    pl.parentElement.nextElementSibling.children[0].innerText = prdTotalPrice;
+    
+  }
+})
